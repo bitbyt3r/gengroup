f2d4fa4feafa1f0f0a1a84c54de8645955800707
mark25@UMBC.EDU <mark25@UMBC.EDU@bb2173db-233b-63cc-8cb0-ec26fdc3f785>
2013-08-21 14:16:22 +0000

    Added missing files

---
+++ push-production.sh
@@ -0,0 +1,6 @@
+svn up
+svn ci -m "Checking in before pushing staging to production"
+rsync --exclude=.svn -ruv rhel6w-complete-staging-x86_64/ rhel6w-complete-x86_64/
+svn ci -m "Pushed staging groups to production"
+./gengroup.py
+
